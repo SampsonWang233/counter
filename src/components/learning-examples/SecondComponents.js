06ed0425dd294fb8bc5f34478c0d3a56c3f98ec1
SampsonWang233 <siyuwang2020@gmail.com>
2022-07-14 15:25:02 -0400

    befor making counter

---
+++ src/components/learning-examples/SecondComponents.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+
+export default function SecondComponent () {
+    return (
+      <div className="secondComponent">
+        SecondComponent
+    </div>
+    );
+  }
+
+export function ThirdComponent () {
+  return (
+    <div className="secondComponent">
+      ThirdComponent
+  </div>
+  );
+}
